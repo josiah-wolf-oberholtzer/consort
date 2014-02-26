b24b05626fca7ee778509be2d854ee7ef49aaba7
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-26 14:21:22 -0500

    Added AttachmentSpecifier to makers/.

---
+++ makers/AttachmentSpecifier.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import abctools
+from experimental.tools import selectortools
+
+
+class AttachmentSpecifier(abctools.AbjadObject):
+    r'''An attachment specifier.
+
+    ::
+
+        >>> from consort import makers
+        >>> attachment_specifier = makers.AttachmentSpecifier(
+        ...     attachments=(indicatortools.Articulation('>'),),
+        ...     selector=selectortools.Selector().by_leaves()[0],
+        ...     )
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_attachments',
+        '_selector',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        attachments=None,
+        selector=None,
+        ):
+        assert isinstance(attachments, tuple)
+        self._attachments = attachments
+        assert isinstance(selector, selectortools.Selector)
+        self._selector = selector
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def attachments(self):
+        return self._attachments
+
+    @property
+    def selector(self):
+        return self._selector
