bf6861cfd17e22f62047ec01be40993341fdb19e
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-26 13:03:50 -0500

    Implemented AlterationAgent.iterate_score() static method.
    NOTE: all agents iterate a score that was previously created, and each agent
    should implement the specific means by which it iterates over that score. Some
    agents iterate logical ties in time-wise order, while others iterate top-level
    count-time containers in each voice. The segment-maker does not need to know
    how they iterate, only that they do.

--- makers/AlterationAgent.py
+++ makers/AlterationAgent.py
@@ -1,5 +1,9 @@
 # -*- encoding: utf-8 -*-
+import collections
 from abjad.tools import abctools
+from abjad.tools import scoretools
+from abjad.tools.topleveltools import inspect_
+from abjad.tools.topleveltools import iterate
 
 
 class AlterationAgent(abctools.AbjadObject):
@@ -20,5 +24,27 @@ class AlterationAgent(abctools.AbjadObject):
 
     ### SPECIAL METHODS ###
 
-    def __call__(self, logical_tie):
+    def __call__(self, logical_tie, seed=0):
         pass
+
+    ### PUBLIC METHODS ###
+
+    @staticmethod
+    def iterate_score(
+        score,
+        progress_indicator=None,
+        ):
+        from consort import makers
+        counter = collections.Counter()
+        for leaf in iterate(score).by_timeline(scoretools.Note):
+            logical_tie = inspect_(leaf).get_logical_tie()
+            if leaf is not logical_tie.head:
+                continue
+            prototype = makers.MusicSpecifier
+            music_specifier = inspect_(leaf).get_effective(prototype)
+            alteration_agent = music_specifier.alteration_agent
+            if alteration_agent is None:
+                continue
+            seed = counter[alteration_agent]
+            alteration_agent(logical_tie, seed=seed)
+            counter[alteration_agent] += 1
