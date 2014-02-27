5c5090c202f33da5545d69db7894faed8ec73159
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-26 19:21:05 -0500

    Fleshed out ContextSpecifier.

--- makers/ContextSpecifier.py
+++ makers/ContextSpecifier.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import abctools
+from abjad.tools import timespantools
 
 
 class ContextSpecifier(abctools.AbjadObject):
@@ -25,3 +26,12 @@ class ContextSpecifier(abctools.AbjadObject):
         self,
         ):
         pass
+
+    ### SPECIAL METHODS ###
+
+    def __call__(
+        self,
+        layer=None,
+        template=None,
+        ):
+        return timespantools.TimespanInventory()
