ab230322920c3e56c5cb681380844400f88ae03e
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-03-15 00:15:55 -0400

    Renamed RegisterCurve to PitchCurve.

--- makers/PitchCurve.py
+++ makers/PitchCurve.py
@@ -7,30 +7,78 @@ from abjad.tools import pitchtools
 from abjad.tools.topleveltools import new
 
 
-class RegisterCurve(abctools.AbjadObject):
+class PitchCurve(abctools.AbjadObject):
+    r'''A pitch curve.
+
+    ::
+
+        >>> from consort import makers
+        >>> pitch_curve = makers.PitchCurve(
+        ...     pitches=(-6, 0, 9),
+        ...     ratio=(2, 1),
+        ...     )
+        >>> print format(pitch_curve)
+        makers.PitchCurve(
+            pitches=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('fs'),
+                    pitchtools.NamedPitch("c'"),
+                    pitchtools.NamedPitch("a'"),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            ratio=mathtools.Ratio(2, 1),
+            )
+
+    ::
+
+        >>> pitch_curve(0)
+        NamedPitch('fs')
+
+    ::
+
+        >>> pitch_curve((1, 3))
+        NamedPitch('a')
+
+    ::
+
+        >>> pitch_curve((1, 2))
+        NamedPitch('b')
+
+    ::
+
+        >>> pitch_curve((2, 3))
+        NamedPitch("c'")
+
+    ::
+
+        >>> pitch_curve(1)
+        NamedPitch("a'")
+
+    '''
 
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_pitches',
         '_ratio',
-        '_registers',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
+        pitches=None,
         ratio=None,
-        registers=None,
         ):
         ratio = mathtools.Ratio([abs(x) for x in ratio])
         self._ratio = ratio
-        assert len(registers) == len(ratio) + 1
-        registers = pitchtools.PitchSegment(
-            registers,
+        assert len(pitches) == len(ratio) + 1
+        pitches = pitchtools.PitchSegment(
+            pitches,
             item_class=pitchtools.NamedPitch,
             )
-        self._registers = registers
+        self._pitches = pitches
 
     ### SPECIAL METHODS ###
 
@@ -41,9 +89,9 @@ class RegisterCurve(abctools.AbjadObject):
         if 1 < position:
             position = durationtools.Offset(1)
         if position == 0:
-            return self.registers[0]
+            return self.pitches[0]
         elif position == 1:
-            return self.registers[-1]
+            return self.pitches[-1]
         ratio_sum = sum(self.ratio)
         positions = [durationtools.Offset(x) / ratio_sum
             for x in mathtools.cumulative_sums(self.ratio)]
@@ -51,8 +99,8 @@ class RegisterCurve(abctools.AbjadObject):
         position = float(position)
         x0 = float(positions[index - 1])
         x1 = float(positions[index])
-        y0 = float(self.registers[index - 1])
-        y1 = float(self.registers[index])
+        y0 = float(self.pitches[index - 1])
+        y1 = float(self.pitches[index])
         dx = x1 - x0
         dy = y1 - y0
         m = float(dy) / float(dx)
@@ -66,15 +114,15 @@ class RegisterCurve(abctools.AbjadObject):
     def transpose(self, expr):
         return new(
             self,
-            registers=self.registers.transpose(expr),
+            pitches=self.pitches.transpose(expr),
             )
 
     ### PUBLIC PROPERTIES ###
 
     @property
-    def ratio(self):
-        return self._ratio
+    def pitches(self):
+        return self._pitches
 
     @property
-    def registers(self):
-        return self._registers
+    def ratio(self):
+        return self._ratio
