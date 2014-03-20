2813ac3ecaf2216d1966dba6c794239e90b70c7c
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-03-20 18:39:41 -0400

    Integrated rhythm-maker materials into segments.

--- segments/rhythm_01/definition.py
+++ segments/rhythm_01/definition.py
@@ -1,35 +1,20 @@
 # -*- encoding: utf-8 -*-
-from abjad import *
+from abjad import new
+from consort import materials
 from consort.segments import time_05 as base
 
 
 voice_specifier_one = new(base.voice_specifier_one,
-    music_specifier__rhythm_maker=rhythmmakertools.TaleaRhythmMaker(
-        talea=rhythmmakertools.Talea(
-            counts=(1, 1),
-            denominator=16,
-            ),
-        tie_specifier=rhythmmakertools.TieSpecifier(
-            tie_split_notes=False,
-            ),
+    music_specifier__rhythm_maker=new(
+        materials.talea_rhythm_maker,
+        extra_counts_per_division=(0,),
+        talea__counts=(1,),
         ),
     )
-
-#voice_specifier_two = new(base.voice_specifier_two,
-#    music_specifier__rhythm_maker=rhythmmakertools.TupletRhythmMaker(
-#        tuplet_ratios=(
-#            (1, 1),
-#            ),
-#        tuplet_spelling_specifier=rhythmmakertools.TupletSpellingSpecifier(
-#            avoid_dots=True,
-#            ),
-#        ),
-#    )
 
 segment_maker = new(base.segment_maker,
     rehearsal_mark='B1',
     voice_specifiers=(
         voice_specifier_one,
-#        voice_specifier_two,
         ),
     )
