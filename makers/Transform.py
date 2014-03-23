c026a8cd799df7899a12e15e5da2a023b7107f45
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-03-23 00:42:11 -0400

    Added Transform stub class.

---
+++ makers/Transform.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+from consort.makers.ConsortObject import ConsortObject
+
+
+class Transform(ConsortObject):
+    r'''A transform.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
