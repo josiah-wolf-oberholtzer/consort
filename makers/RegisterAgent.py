4552320a3170279f2c7ebefdcc7991187cb53614
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-03-23 01:02:23 -0400

    Simplified RegisterAgent.

--- makers/RegisterAgent.py
+++ makers/RegisterAgent.py
@@ -1,9 +1,11 @@
 # -*- encoding: utf-8 -*-
 import collections
-from consort.makers.ConsortObject import ConsortObject
+from abjad.tools import datastructuretools
+from abjad.tools import pitchtools
 from abjad.tools import scoretools
 from abjad.tools.topleveltools import inspect_
 from abjad.tools.topleveltools import iterate
+from consort.makers.ConsortObject import ConsortObject
 
 
 class RegisterAgent(ConsortObject):
@@ -13,23 +15,18 @@ class RegisterAgent(ConsortObject):
     ### CLASS VARIABLES ###
 
     __slots__ = (
-        '_division_inflections',
-        '_global_inflections',
-        '_octavations',
-        '_phrase_inflections',
+        '_register',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
-        division_inflections=None,
-        global_inflections=None,
-        octavations=None,
-        phrase_inflections=None,
+        register=None,
         ):
-        from plague_water import makers
-        pass
+        if register is not None:
+            register = pitchtools.NamedPitch(register)
+        self._register = register
 
     ### SPECIAL METHODS ###
 
@@ -39,6 +36,17 @@ class RegisterAgent(ConsortObject):
         seed=0,
         ):
         assert isinstance(music, scoretools.Container)
+        octave_transposition_mapping = self.octave_transposition_mapping
+        iterator = iterate(music).by_logical_tie(pitched=True)
+        for i, logical_tie in enumerate(iterator):
+            i += seed
+            octave = pitchtools.Octave(self.octavations[i])
+            named_pitch = logical_tie.head.written_pitch
+            named_pitch_class = named_pitch.named_pitch_class
+            octavated_pitch = pitchtools.NamedPitch(named_pitch_class, octave)
+            new_pitch = octave_transposition_mapping([octavated_pitch])
+            for note in logical_tie:
+                note.written_pitch = new_pitch
 
     ### PUBLIC METHODS ###
 
@@ -55,27 +63,29 @@ class RegisterAgent(ConsortObject):
                 register_agent = music_specifier.register_agent
                 if register_agent is None:
                     continue
-                seed = counter[register_agent]
+                seed = counter[music_specifier]
                 register_agent(
                     container,
                     seed=seed,
                     )
-                counter[register_agent] += 1
+                counter[music_specifier] += 1
 
     ### PUBLIC PROPERTIES ###
 
     @property
-    def division_inflections(self):
-        return self._division_inflections
-
-    @property
-    def global_inflections(self):
-        return self._global_inflections
-
-    @property
-    def phrase_inflections(self):
-        return self._phrase_inflections
-
-    @property
     def octavations(self):
-        return self._octavations
+        return datastructuretools.CyclicTuple((
+            2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4, 5, 9, 0, 4, 5, 2, 3, 5, 3, 6, 0,
+            2, 8, 7, 4, 7, 1, 3, 5, 2, 6, 6, 2, 4, 9, 7, 7, 5, 7, 2, 4, 7, 0,
+            ))
+
+    @property
+    def register(self):
+        return self._register
+
+    @property
+    def octave_transposition_mapping(self):
+        return pitchtools.OctaveTranspositionMapping([
+            ('[C0, C5)', self.register),
+            ('[C5, C10)', self.register + 6),
+            ])
