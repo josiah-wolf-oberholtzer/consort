3616fcd2249816c2730ec32b1a66783e3d43af86
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-04-27 13:15:57 -0400

    Implemented bow-direction-change indicators on BowSpanner.

--- makers/BowSpanner.py
+++ makers/BowSpanner.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+from abjad import *
 from abjad.tools import indicatortools
 from abjad.tools import lilypondnametools
 from abjad.tools import schemetools
@@ -13,7 +14,6 @@ class BowSpanner(spannertools.Spanner):
 
         >>> from consort import makers
         >>> staff = Staff("c'4 c'4 c'4 c'4")
-        >>> attach(makers.BowSpanner(), staff[:])
 
     ::
 
@@ -24,34 +24,42 @@ class BowSpanner(spannertools.Spanner):
 
     ::
 
+        >>> attach(makers.BowSpanner(), staff[:])
+
+    ::
+
         >>> print(format(staff))
         \new Staff {
+            \once \override NoteHead.Y-offset = -1.0
             \once \override NoteHead.stencil = #ly:text-interface::print
-            \once \override NoteHead.text = ^ \markup {
+            \once \override NoteHead.text = \markup {
                 \vcenter
                     \fraction
                         1
                         4
                 }
-            c'4 \glissando
+            c'4 ^\upbow \glissando
+            \once \override NoteHead.Y-offset = 1.0
             \once \override NoteHead.stencil = #ly:text-interface::print
-            \once \override NoteHead.text = ^ \markup {
+            \once \override NoteHead.text = \markup {
                 \vcenter
                     \fraction
                         3
                         4
                 }
-            c'4 \glissando
+            c'4 ^\downbow \glissando
+            \once \override NoteHead.Y-offset = 0.0
             \once \override NoteHead.stencil = #ly:text-interface::print
-            \once \override NoteHead.text = ^ \markup {
+            \once \override NoteHead.text = \markup {
                 \vcenter
                     \fraction
                         1
                         2
                 }
-            c'4 \glissando
+            c'4 ^\upbow \glissando
+            \once \override NoteHead.Y-offset = 2.0
             \once \override NoteHead.stencil = #ly:text-interface::print
-            \once \override NoteHead.text = ^ \markup {
+            \once \override NoteHead.text = \markup {
                 \vcenter
                     \fraction
                         1
@@ -120,6 +128,11 @@ class BowSpanner(spannertools.Spanner):
             )
         if not self._is_my_last_leaf(leaf):
             lilypond_format_bundle.right.spanner_starts.append(r'\glissando')
+            self._make_bow_direction_change_contributions(
+                bow_hair_contact_point=bow_hair_contact_point,
+                leaf=leaf,
+                lilypond_format_bundle=lilypond_format_bundle,
+                )
             self._make_glissando_overrides(
                 bow_pressure=bow_pressure,
                 bow_technique=bow_technique,
@@ -127,6 +140,40 @@ class BowSpanner(spannertools.Spanner):
                 string_contact_point=string_contact_point,
                 )
         return lilypond_format_bundle
+
+    def _make_bow_direction_change_contributions(
+        self,
+        bow_hair_contact_point=None,
+        leaf=None,
+        lilypond_format_bundle=None,
+        ):
+        direction_change = None
+        next_leaf = inspect_(leaf).get_leaf(1)
+        this_contact_point = bow_hair_contact_point
+        next_contact_point = inspect_(next_leaf).get_indicator(
+            indicatortools.BowHairContactPoint)
+        if self._is_my_first_leaf(leaf):
+            if this_contact_point < next_contact_point:
+                direction_change = Up
+            elif next_contact_point < this_contact_point:
+                direction_change = Down
+        else:
+            previous_leaf = inspect_(leaf).get_leaf(-1)
+            previous_contact_point = inspect_(previous_leaf
+                ).get_indicator(indicatortools.BowHairContactPoint)
+            if previous_contact_point < this_contact_point and \
+                next_contact_point < this_contact_point:
+                direction_change = Down
+            elif this_contact_point < previous_contact_point and \
+                this_contact_point < next_contact_point:
+                direction_change = Up
+        if direction_change is None:
+            return
+        if direction_change is Up:
+            articulation = indicatortools.Articulation('upbow', Up)
+        elif direction_change is Down:
+            articulation = indicatortools.Articulation('downbow', Up)
+        lilypond_format_bundle.right.articulations.append(str(articulation))
 
     def _make_bow_hair_contact_point_overrides(
         self,
@@ -139,17 +186,27 @@ class BowSpanner(spannertools.Spanner):
             property_path='stencil',
             value=schemetools.Scheme('ly:text-interface::print'),
             )
+        stencil_override_string = '\n'.join(
+            stencil_override.override_format_pieces)
+        lilypond_format_bundle.grob_overrides.append(stencil_override_string)
         text_override = lilypondnametools.LilyPondGrobOverride(
             grob_name='NoteHead',
             is_once=True,
             property_path='text',
             value=bow_hair_contact_point.markup,
             )
-        stencil_override_string = '\n'.join(
-            stencil_override.override_format_pieces)
         text_override_string = '\n'.join(text_override.override_format_pieces)
-        lilypond_format_bundle.grob_overrides.append(stencil_override_string)
         lilypond_format_bundle.grob_overrides.append(text_override_string)
+        y_offset = float((4 * bow_hair_contact_point.contact_point) - 2)
+        y_offset_override = lilypondnametools.LilyPondGrobOverride(
+            grob_name='NoteHead',
+            is_once=True,
+            property_path='Y-offset',
+            value=y_offset,
+            )
+        y_offset_override_string = '\n'.join(
+            y_offset_override.override_format_pieces)
+        lilypond_format_bundle.grob_overrides.append(y_offset_override_string)
 
     def _make_glissando_overrides(
         self,
