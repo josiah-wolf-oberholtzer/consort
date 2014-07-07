e0d846267c1ab065c144f49bc9def50df643fbe7
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-07 13:54:34 -0400

    consort ...

--- __metadata__.py
+++ __metadata__.py
@@ -3,9 +3,10 @@ import collections
 
 
 metadata = collections.OrderedDict([
+    ('composer', 'Oberholtzer'),
     ('forces_tagline', 'for String Orchestra'),
-    ('title', 'Consort'),
-    ('year', 2014),
     ('locale', 'Jamaica Plain'),
     ('time_period', ('February 2014', 'April 2014')),
+    ('title', 'Consort'),
+    ('year', 2014),
     ])
