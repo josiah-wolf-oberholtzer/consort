ad2e68360a61f335c81109b064c05cbf7c69abf2
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-09 18:40:25 +0200

    Improved HarmonicFieldPitchMaker.

--- makers/HarmonicFieldPitchMaker.py
+++ makers/HarmonicFieldPitchMaker.py
@@ -1,6 +1,9 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import abctools
+from abjad.tools import pitchtools
 from abjad.tools import sequencetools
+from abjad.tools.topleveltools import inspect_
+from abjad.tools.topleveltools import iterate
 
 
 class HarmonicFieldPitchMaker(abctools.AbjadValueObject):
@@ -10,6 +13,8 @@ class HarmonicFieldPitchMaker(abctools.AbjadValueObject):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_allow_repetition',
+        '_chord_specifiers',
         '_harmonic_fields',
         '_register_specifier',
         )
@@ -18,11 +23,13 @@ class HarmonicFieldPitchMaker(abctools.AbjadValueObject):
 
     def __init__(
         self,
+        allow_repetition=False,
         chord_specifiers=None,
         harmonic_fields=None,
         register_specifier=None,
         ):
         from consort import makers
+        self._allow_repetition = bool(allow_repetition)
         prototype = makers.ChordSpecifier
         if isinstance(chord_specifiers, prototype):
             chord_specifiers = (chord_specifiers,)
@@ -42,6 +49,49 @@ class HarmonicFieldPitchMaker(abctools.AbjadValueObject):
     ### SPECIAL METHODS ###
 
     def __call__(
+        self,
+        music,
+        seed=0,
+        ):
+        pitch_range = inspect_(music).get_effective(pitchtools.PitchRange)
+        previous_pitch = None
+        iterator = iterate(music).by_logical_tie(pitched=True)
+        for i, logical_tie in enumerate(iterator):
+            previous_pitch = self._process_logical_tie(
+                logical_tie,
+                previous_pitch=previous_pitch,
+                pitch_range=pitch_range,
+                seed=seed + i,
+                )
+
+    ### PRIVATE METHODS ###
+
+    def _process_grace_notes(
+        self,
+        logical_tie,
+        harmonic_field_entry,
+        pitch_range,
+        ):
+        previous_leaf = logical_tie.head._get_leaf(-1)
+        if not previous_leaf:
+            return
+        grace_containers = inspect_(previous_leaf).get_grace_containers('after')
+        if not grace_containers:
+            return
+        after_grace = grace_containers[0]
+        leaves = reversed(after_grace.select_leaves())
+        pitches = harmonic_field_entry.leading_pitches + \
+            (harmonic_field_entry.structural_pitch,)
+        pitches = pitches.retrograde()
+        for i, leaf in enumerate(leaves, 1):
+            pitch = pitches[i]
+            if pitch < pitch_range.start_pitch:
+                pitch = pitch.transpose(12)
+            elif pitch_range.stop_pitch < pitch:
+                pitch = pitch.transpose(-12)
+            leaf.written_pitch = pitch
+
+    def _process_logical_tie(
         self,
         logical_tie,
         previous_pitch,
@@ -65,19 +115,27 @@ class HarmonicFieldPitchMaker(abctools.AbjadValueObject):
             entry_count=2,
             pitch_range=pitch_range,
             )
-        resulting_pitch = harmonic_field_entries[0]
-        if resulting_pitch == previous_pitch:
-            resulting_pitch = harmonic_field_entries[1]
+        harmonic_field_entry = harmonic_field_entries[0]
+        structural_pitch = harmonic_field_entry.structural_pitch
+        if not self.allow_repetition:
+            if structural_pitch == previous_pitch:
+                structural_pitch = harmonic_field_entries[1]
         for note in logical_tie:
-            note.written_pitch = resulting_pitch
+            note.written_pitch = structural_pitch
         if self.chord_specifiers:
             chord_specifier = self.chord_specifiers[seed]
             chord_specifier(
                 logical_tie,
                 pitch_range=pitch_range,
                 )
+        self._process_grace_notes(logical_tie, harmonic_field_entry)
+        return structural_pitch
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def allow_repetition(self):
+        return self._allow_repetition
 
     @property
     def chord_specifiers(self):
