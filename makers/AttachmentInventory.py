6dd792e9f5cf3fcc0282c0ee85fd0612db61df0e
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-15 11:15:13 -0400

    Added AttachmentInventory.

---
+++ makers/AttachmentInventory.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import datastructuretools
+
+
+class AttachmentInventory(datastructuretools.TypedList):
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
