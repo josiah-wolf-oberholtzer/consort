f0dbf544aa7e028d9f49a2d957e457206bc97455
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-18 10:45:50 -0400

    Fleshed out PitchMaker.

--- makers/PitchMaker.py
+++ makers/PitchMaker.py
@@ -1,9 +1,46 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import abctools
+from abjad.tools import pitchtools
+from abjad.tools.topleveltools import inspect_
+from abjad.tools.topleveltools import iterate
 
 
 class PitchMaker(abctools.AbjadValueObject):
     
     ### CLASS VARIABLES ###
 
-    __slots__ = ()
+    __slots__ = (
+        '_allow_repetition',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        allow_repetition=False,
+        ):
+        self._allow_repetition = bool(allow_repetition)
+
+    ### SPECIAL METHODS ###
+
+    def __call__(
+        self,
+        music,
+        seed=0,
+        ):
+        pitch_range = inspect_(music).get_effective(pitchtools.PitchRange)
+        previous_pitch = None
+        iterator = iterate(music).by_logical_tie(pitched=True)
+        for i, logical_tie in enumerate(iterator):
+            previous_pitch = self._process_logical_tie(
+                logical_tie,
+                previous_pitch=previous_pitch,
+                pitch_range=pitch_range,
+                seed=seed + i,
+                )
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def allow_repetition(self):
+        return self._allow_repetition
