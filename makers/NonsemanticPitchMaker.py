3b2ae35357810e8966da3819b56369ad4da32649
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-18 10:45:59 -0400

    Refactored NonsemanticPitchMaker around PitchMaker.

--- makers/NonsemanticPitchMaker.py
+++ makers/NonsemanticPitchMaker.py
@@ -1,34 +1,42 @@
 # -*- encoding: utf-8 -*-
-from abjad.tools import abctools
-from abjad.tools.topleveltools import iterate
+from abjad.tools import datastructuretools
+from abjad.tools import pitchtools
+from abjad.tools import scoretools
+from abjad.tools.topleveltools import attach
+from abjad.tools.topleveltools import detach
+from abjad.tools.topleveltools import inspect_
+from abjad.tools.topleveltools import mutate
+from consort.makers.PitchMaker import PitchMaker
 
 
-class NonsemanticPitchMaker(abctools.AbjadValueObject):
+class NonsemanticPitchMaker(PitchMaker):
 
     ### CLASS VARIABLES ###
 
-    __slots__ = ()
+    __slots__ = (
+        '_pitches',
+        )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
+        allow_repetition=None,
+        groupings=None,
+        pitches=None,
         ):
-        pass
-
-    ### SPECIAL METHODS ###
-
-    def __call__(
-        self,
-        music,
-        seed=0,
-        ):
-        iterator = iterate(music).by_logical_tie(pitched=True)
-        for i, logical_tie in enumerate(iterator):
-            self._process_logical_tie(
-                logical_tie,
-                seed=seed + i,
-                )
+        PitchMaker.__init__(
+            self,
+            allow_repetition=allow_repetition,
+            )
+        groupings = groupings or (1,)
+        groupings = tuple(int(x) for x in groupings)
+        assert all(0 < x for x in groupings)
+        self._groupings = groupings
+        pitches = pitchtools.PitchSegment(pitches)
+        assert pitches
+        pitches = datastructuretools.CyclicTuple(pitches)
+        self._pitches = pitches
 
     ### PRIVATE METHODS ###
 
@@ -37,4 +45,32 @@ class NonsemanticPitchMaker(abctools.AbjadValueObject):
         logical_tie,
         seed=0,
         ):
-        pass
+        grouping = self._groupings[seed]
+        indices = range(grouping, seed)
+        pitches = [self.pitches[x] for x in indices]
+        pitches = pitchtools.PitchSet(pitches)
+        for i, leaf in enumerate(logical_tie):
+            chord = scoretools.Chord(leaf)
+            chord.written_pitches = pitches
+            grace_containers = inspect_(leaf).get_grace_containers('after')
+            if grace_containers:
+                old_grace_container = grace_containers[0]
+                grace_notes = old_grace_container.select_leaves()
+                detach(scoretools.GraceContainer, leaf)
+            mutate(leaf).replace(chord)
+            if grace_containers:
+                new_grace_container = scoretools.GraceContainer(
+                    grace_notes,
+                    kind='after',
+                    )
+                attach(new_grace_container, chord)
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def groupings(self):
+        return self._groupings
+
+    @property
+    def pitches(self):
+        return self._pitches
