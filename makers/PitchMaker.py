3819d7a41d85e7182668a8d58e0cfe824da5e6d4
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-19 13:48:50 -0400

    Integrated chord expressions into PitchMaker.

--- makers/PitchMaker.py
+++ makers/PitchMaker.py
@@ -1,16 +1,18 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import abctools
+from abjad.tools import datastructuretools
 from abjad.tools import pitchtools
 from abjad.tools.topleveltools import inspect_
 from abjad.tools.topleveltools import iterate
 
 
 class PitchMaker(abctools.AbjadValueObject):
-    
+
     ### CLASS VARIABLES ###
 
     __slots__ = (
         '_allow_repetition',
+        '_chord_expressions',
         )
 
     ### INITIALIZER ###
@@ -18,8 +20,21 @@ class PitchMaker(abctools.AbjadValueObject):
     def __init__(
         self,
         allow_repetition=False,
+        chord_expressions=None,
         ):
+        from consort import makers
         self._allow_repetition = bool(allow_repetition)
+        if chord_expressions is not None:
+            prototype = (
+                makers.ChordExpression,
+                makers.KeyClusterExpression,
+                )
+            assert chord_expressions
+            assert all(isinstance(x, prototype) for x in chord_expressions)
+            chord_expressions = datastructuretools.CyclicTuple(
+                chord_expressions,
+                )
+        self._chord_expressions = chord_expressions
 
     ### SPECIAL METHODS ###
 
@@ -39,8 +54,19 @@ class PitchMaker(abctools.AbjadValueObject):
                 seed=seed + i,
                 )
 
+    ### PRIVATE METHODS ###
+
+    def _apply_chord_expression(self, logical_tie, seed=0):
+        if self.chord_expressions:
+            chord_expression = self.chord_expressions[seed]
+            chord_expression(logical_tie)
+
     ### PUBLIC PROPERTIES ###
 
     @property
     def allow_repetition(self):
         return self._allow_repetition
+
+    @property
+    def chord_expressions(self):
+        return self._chord_expressions
