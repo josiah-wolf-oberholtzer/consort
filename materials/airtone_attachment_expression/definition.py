f0c030d7449e195583ea1945efbdb81d287e6caa
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-21 15:48:53 -0400

    Moved all remaining materials into zaira.

--- materials/airtone_attachment_expression/definition.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad import *
-import consort
-
-
-airtone_attachment_expression = consort.makers.AttachmentExpression(
-    attachments=spannertools.StemTremoloSpanner(),
-    selector=selectortools.selects_pitched_runs(),
-    )
