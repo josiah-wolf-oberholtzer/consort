e1640cd6909c7ce90d4d4ee5538089a3ac4927bf
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-01 18:25:05 -0800

    Reorganized consort for packaging.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+install_requires = (
+    'abjad',
+    'supriya',
+    )
+
+
+def main():
+    setup(
+        author='Josiah Wolf Oberholtzer',
+        author_email='josiah.oberholtzer@gmail.com',
+        install_requires=install_requires,
+        name='consort',
+        packages=('consort',),
+        url='https://github.com/josiah-wolf-oberholtzer/consort',
+        version='0.1',
+        zip_safe=False,
+        )
+
+
+if __name__ == '__main__':
+    main()
