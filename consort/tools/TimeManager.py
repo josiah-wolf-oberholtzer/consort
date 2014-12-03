2c5781d8e26245aa7ca5e32a5d24ca56e6254a1f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-02 23:19:54 -0800

    Outlined TimeManager workflow.

--- consort/tools/TimeManager.py
+++ consort/tools/TimeManager.py
@@ -12,4 +12,37 @@ class TimeManager(abctools.AbjadValueObject):
         score_template=None,
         settings=None,
         ):
-        pass
+        
+        score = score_template()
+
+        # make independent timespans
+        timespan_inventory = TimespanManager._make_multiplexed_timespans(
+            dependent=False,
+            score=score,
+            score_template=score_template,
+            settings=settings,
+            target_duration=target_duration,
+            )
+
+        # find meters
+
+        # split performed timespans by meter offsets
+
+        # group performed timespans by music specifier
+
+        # inscribe grouped performed timespans with rhythms
+
+            # rebuild performed timespans without silence divisions
+            # (the inscription process should do the rebuilding)
+
+        # make dependent timespans
+
+        # make (pre-split) silent timespans
+
+        # rewrite meters? (magic)
+
+        # populate score
+
+        # perform other rhythmic processing
+
+        # collect attack points
