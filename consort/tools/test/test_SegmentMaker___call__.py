2e260a3e41997ac39e8a8fd1fd8ab0a968674a29
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-04 13:00:59 -0800

    Wrote SegmentMaker.__call__() tests.

--- consort/tools/test/test_SegmentMaker___call__.py
+++ consort/tools/test/test_SegmentMaker___call__.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf -*-
 from abjad.tools import durationtools
 from abjad.tools import indicatortools
+from abjad.tools import rhythmmakertools
 from abjad.tools import systemtools
 from abjad.tools import templatetools
 import consort
@@ -295,6 +296,106 @@ def test_SegmentMaker___call___04():
                             {
                                 {
                                     c'4..
+                                    \bar "||"
+                                }
+                            }
+                        }
+                    }
+                >>
+            >>
+        }
+        '''), format(lilypond_file)
+
+
+def test_SegmentMaker___call___05():
+    segment_maker = consort.SegmentMaker(
+        discard_final_silence=True,
+        duration_in_seconds=3,
+        omit_stylesheets=True,
+        score_template=templatetools.GroupedRhythmicStavesScoreTemplate(
+            staff_count=2,
+            ),
+        settings=(
+            consort.MusicSetting(
+                timespan_maker=consort.TaleaTimespanMaker(
+                    initial_silence_talea=rhythmmakertools.Talea(
+                        counts=(0, 3),
+                        denominator=16,
+                        ),
+                    silence_talea=rhythmmakertools.Talea(
+                        counts=(1,),
+                        denominator=8,
+                        ),
+                    minimum_duration=durationtools.Duration(1, 8),
+                    ),
+                v1=consort.MusicSpecifier(),
+                v2=consort.MusicSpecifier(),
+                ),
+            ),
+        tempo=indicatortools.Tempo((1, 4), 60),
+        permitted_time_signatures=((5, 8), (7, 16)),
+        )
+    lilypond_file = segment_maker()
+    assert systemtools.TestManager.compare(
+        format(lilypond_file),
+        r'''
+        \version "2.19.15"
+        \language "english"
+
+        \score {
+            \context Score = "Grouped Rhythmic Staves Score" <<
+                \tag time
+                \context TimeSignatureContext = "TimeSignatureContext" {
+                    {
+                        \time 7/16
+                        \tempo 4=60
+                        s1 * 7/16
+                    }
+                    {
+                        s1 * 7/16
+                    }
+                }
+                \context StaffGroup = "Grouped Rhythmic Staves Staff Group" <<
+                    \context RhythmicStaff = "Staff 1" {
+                        \context Voice = "Voice 1" {
+                            {
+                                {
+                                    c'4
+                                }
+                            }
+                            {
+                                {
+                                    r8.
+                                }
+                            }
+                            {
+                                {
+                                    c'8.
+                                }
+                            }
+                            {
+                                {
+                                    r4
+                                    \bar "||"
+                                }
+                            }
+                        }
+                    }
+                    \context RhythmicStaff = "Staff 2" {
+                        \context Voice = "Voice 2" {
+                            {
+                                {
+                                    r8.
+                                }
+                            }
+                            {
+                                {
+                                    c'4
+                                }
+                            }
+                            {
+                                {
+                                    r4..
                                     \bar "||"
                                 }
                             }
