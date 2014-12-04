e0d9be0fea0caa1b94b97d94085d4d5ecd5ff423
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-04 12:12:14 -0800

    Wrote SegmentMaker.__call__() tests.

--- consort/tools/test/test_SegmentMaker___call__.py
+++ consort/tools/test/test_SegmentMaker___call__.py
@@ -1,35 +1,158 @@
 # -*- encoding: utf -*-
 from abjad.tools import indicatortools
+from abjad.tools import systemtools
 from abjad.tools import templatetools
 import consort
 
 
 def test_SegmentMaker___call___01():
-
-    score_template = templatetools.StringOrchestraScoreTemplate(
-        violin_count=2,
-        viola_count=1,
-        cello_count=1,
-        contrabass_count=0,
-        )
-
     segment_maker = consort.SegmentMaker(
+        discard_final_silence=True,
         duration_in_seconds=2,
-        score_template=score_template,
+        include_stylesheets=False,
+        score_template=templatetools.GroupedRhythmicStavesScoreTemplate(
+            staff_count=2,
+            ),
         settings=(
             consort.MusicSetting(
                 timespan_maker=consort.TaleaTimespanMaker(),
-                violin_1_bowing_voice=None,
-                violin_2_bowing_voice=None,
+                v1=consort.MusicSpecifier(),
+                v2=consort.MusicSpecifier(),
                 ),
             ),
         tempo=indicatortools.Tempo((1, 4), 60),
-        permitted_time_signatures=(
-            (5, 8),
-            (7, 16),
-            ),
+        permitted_time_signatures=((5, 8), (7, 16)),
         )
+    lilypond_file = segment_maker()
+    assert systemtools.TestManager.compare(
+        format(lilypond_file),
+        r'''
+        \version "2.19.15"
+        \language "english"
 
-    result = segment_maker()
+        \score {
+            \context Score = "Grouped Rhythmic Staves Score" <<
+                \tag time
+                \context TimeSignatureContext = "TimeSignatureContext" {
+                    {
+                        \time 7/16
+                        \tempo 4=60
+                        s1 * 7/16
+                    }
+                }
+                \context StaffGroup = "Grouped Rhythmic Staves Staff Group" <<
+                    \context RhythmicStaff = "Staff 1" {
+                        \context Voice = "Voice 1" {
+                            {
+                                {
+                                    c'4
+                                }
+                            }
+                            {
+                                {
+                                    r8.
+                                    \bar "||"
+                                }
+                            }
+                        }
+                    }
+                    \context RhythmicStaff = "Staff 2" {
+                        \context Voice = "Voice 2" {
+                            {
+                                {
+                                    c'4
+                                }
+                            }
+                            {
+                                {
+                                    r8.
+                                    \bar "||"
+                                }
+                            }
+                        }
+                    }
+                >>
+            >>
+        }
+        '''), format(lilypond_file)
 
-    assert result is not None
+
+def test_SegmentMaker___call___02():
+    segment_maker = consort.SegmentMaker(
+        discard_final_silence=False,
+        duration_in_seconds=2,
+        include_stylesheets=False,
+        score_template=templatetools.GroupedRhythmicStavesScoreTemplate(
+            staff_count=2,
+            ),
+        settings=(
+            consort.MusicSetting(
+                timespan_maker=consort.TaleaTimespanMaker(),
+                v1=consort.MusicSpecifier(),
+                v2=consort.MusicSpecifier(),
+                ),
+            ),
+        tempo=indicatortools.Tempo((1, 4), 60),
+        permitted_time_signatures=((5, 8), (7, 16)),
+        )
+    lilypond_file = segment_maker()
+    assert systemtools.TestManager.compare(
+        format(lilypond_file),
+        r'''
+        \version "2.19.15"
+        \language "english"
+
+        \score {
+            \context Score = "Grouped Rhythmic Staves Score" <<
+                \tag time
+                \context TimeSignatureContext = "TimeSignatureContext" {
+                    {
+                        \time 7/16
+                        \tempo 4=60
+                        s1 * 7/16
+                    }
+                    {
+                        s1 * 7/16
+                    }
+                }
+                \context StaffGroup = "Grouped Rhythmic Staves Staff Group" <<
+                    \context RhythmicStaff = "Staff 1" {
+                        \context Voice = "Voice 1" {
+                            {
+                                {
+                                    c'4
+                                }
+                            }
+                            {
+                                {
+                                    r8.
+                                }
+                                {
+                                    r4..
+                                    \bar "||"
+                                }
+                            }
+                        }
+                    }
+                    \context RhythmicStaff = "Staff 2" {
+                        \context Voice = "Voice 2" {
+                            {
+                                {
+                                    c'4
+                                }
+                            }
+                            {
+                                {
+                                    r8.
+                                }
+                                {
+                                    r4..
+                                    \bar "||"
+                                }
+                            }
+                        }
+                    }
+                >>
+            >>
+        }
+        '''), format(lilypond_file)
