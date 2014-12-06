b11125e3328b3d78be026951638d314e46a2d2f7
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-06 15:59:40 -0800

    Wrote GraceHandler tests.

--- consort/tools/test/test_GraceHandler.py
+++ consort/tools/test/test_GraceHandler.py
@@ -190,7 +190,7 @@ def test_GraceHandler_02():
         r'''
         \version "2.19.15"
         \language "english"
-        
+
         \score {
             \context Score = "Grouped Rhythmic Staves Score" <<
                 \tag time
@@ -295,6 +295,154 @@ def test_GraceHandler_02():
                                         \override Flag #'stroke-style = #"grace"
                                         \override Script #'font-size = #0.5
                                         \override Stem #'length = #8
+                                        c'16
+                                        c'16
+                                        c'16
+                                        \revert Flag #'stroke-style
+                                        \revert Script #'font-size
+                                        \revert Stem #'length
+                                    }
+                                }
+                                {
+                                    c'4
+                                    \bar "||"
+                                }
+                            }
+                        }
+                    }
+                >>
+            >>
+        }
+        '''), format(lilypond_file)
+
+
+def test_GraceHandler_03():
+    music_specifier = consort.MusicSpecifier(
+        grace_handler=consort.GraceHandler(
+            counts=(0, 2, 4),
+            ),
+        rhythm_maker=rhythmmakertools.NoteRhythmMaker(
+            tie_specifier=rhythmmakertools.TieSpecifier(
+                tie_across_divisions=False,
+                ),
+            ),
+        )
+    segment_maker = consort.SegmentMaker(
+        discard_final_silence=True,
+        duration_in_seconds=4,
+        omit_stylesheets=True,
+        score_template=templatetools.GroupedRhythmicStavesScoreTemplate(
+            staff_count=2,
+            ),
+        settings=(
+            consort.MusicSetting(
+                timespan_maker=consort.TaleaTimespanMaker(
+                    initial_silence_talea=rhythmmakertools.Talea(
+                        counts=(1,),
+                        denominator=4,
+                        ),
+                    playing_groupings=(3,),
+                    ),
+                v1=music_specifier,
+                v2=music_specifier,
+                ),
+            ),
+        tempo=indicatortools.Tempo((1, 4), 60),
+        permitted_time_signatures=((4, 4),),
+        )
+    lilypond_file = segment_maker()
+    assert systemtools.TestManager.compare(
+        format(lilypond_file),
+        r'''
+        \version "2.19.15"
+        \language "english"
+
+        \score {
+            \context Score = "Grouped Rhythmic Staves Score" <<
+                \tag time
+                \context TimeSignatureContext = "TimeSignatureContext" {
+                    {
+                        \time 4/4
+                        \tempo 4=60
+                        s1 * 1
+                    }
+                }
+                \context StaffGroup = "Grouped Rhythmic Staves Staff Group" <<
+                    \context RhythmicStaff = "Staff 1" {
+                        \context Voice = "Voice 1" {
+                            {
+                                {
+                                    r4
+                                }
+                            }
+                            {
+                                {
+                                    \afterGrace
+                                    c'4
+                                    {
+                                        \override Flag #'stroke-style = #"grace"
+                                        \override Script #'font-size = #0.5
+                                        \override Stem #'length = #8
+                                        c'16
+                                        c'16
+                                        c'16
+                                        c'16
+                                        \revert Flag #'stroke-style
+                                        \revert Script #'font-size
+                                        \revert Stem #'length
+                                    }
+                                }
+                                {
+                                    \afterGrace
+                                    c'4
+                                    {
+                                        \override Flag #'stroke-style = #"grace"
+                                        \override Script #'font-size = #0.5
+                                        \override Stem #'length = #8
+                                        c'16
+                                        c'16
+                                        \revert Flag #'stroke-style
+                                        \revert Script #'font-size
+                                        \revert Stem #'length
+                                    }
+                                }
+                                {
+                                    c'4
+                                    \bar "||"
+                                }
+                            }
+                        }
+                    }
+                    \context RhythmicStaff = "Staff 2" {
+                        \context Voice = "Voice 2" {
+                            {
+                                {
+                                    \afterGrace
+                                    r4
+                                    {
+                                        \override Flag #'stroke-style = #"grace"
+                                        \override Script #'font-size = #0.5
+                                        \override Stem #'length = #8
+                                        c'16
+                                        c'16
+                                        \revert Flag #'stroke-style
+                                        \revert Script #'font-size
+                                        \revert Stem #'length
+                                    }
+                                }
+                            }
+                            {
+                                {
+                                    c'4
+                                }
+                                {
+                                    \afterGrace
+                                    c'4
+                                    {
+                                        \override Flag #'stroke-style = #"grace"
+                                        \override Script #'font-size = #0.5
+                                        \override Stem #'length = #8
+                                        c'16
                                         c'16
                                         c'16
                                         c'16
