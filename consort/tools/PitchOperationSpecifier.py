acf67e35643730702aaf497afb24fa2a9338117e
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-20 15:33:45 -0500

    Streamlined PitchOperationSpecifier.

--- consort/tools/PitchOperationSpecifier.py
+++ consort/tools/PitchOperationSpecifier.py
@@ -1,5 +1,4 @@
 # -*- encoding: utf-8 -*-
-from abjad import new
 from abjad import abctools
 from abjad import mathtools
 from abjad import timespantools
@@ -84,9 +83,56 @@ class PitchOperationSpecifier(abctools.AbjadValueObject):
         assert len(ratio) == len(pitch_operations)
         self._ratio = ratio
 
-    ### PRIVATE METHODS ###
+    ### PUBLIC METHODS ###
 
-    def _get_timespans(self, stop_offset):
+    def get_timespans(self, stop_offset):
+        r'''Gets pitch expr timespans.
+
+        ::
+
+            >>> timespans = pitch_operation_specifier.get_timespans(10)
+            >>> print(format(timespans))
+            supriya.tools.timetools.TimespanCollection(
+                [
+                    timespantools.AnnotatedTimespan(
+                        start_offset=durationtools.Offset(0, 1),
+                        stop_offset=durationtools.Offset(5, 2),
+                        annotation=consort.tools.PitchOperation(
+                            operators=(
+                                pitchtools.Rotation(
+                                    index=1,
+                                    transpose=True,
+                                    ),
+                                pitchtools.Transposition(
+                                    index=1,
+                                    ),
+                                ),
+                            ),
+                        ),
+                    timespantools.AnnotatedTimespan(
+                        start_offset=durationtools.Offset(5, 2),
+                        stop_offset=durationtools.Offset(15, 2),
+                        ),
+                    timespantools.AnnotatedTimespan(
+                        start_offset=durationtools.Offset(15, 2),
+                        stop_offset=durationtools.Offset(10, 1),
+                        annotation=consort.tools.PitchOperation(
+                            operators=(
+                                pitchtools.Rotation(
+                                    index=-1,
+                                    transpose=True,
+                                    ),
+                                pitchtools.Transposition(
+                                    index=-1,
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ]
+                )
+
+        Returns timespan colleciton.
+        '''
         timespans = timetools.TimespanCollection()
         target_timespan = timespantools.Timespan(
             start_offset=0,
@@ -102,78 +148,6 @@ class PitchOperationSpecifier(abctools.AbjadValueObject):
                 )
             timespans.insert(annotated_timespan)
         return timespans
-
-    ### PUBLIC METHODS ###
-
-    def get_timespans(self, pitch_expr, stop_offset):
-        r'''Gets pitch expr timespans.
-
-        ::
-
-            >>> pitch_expr = pitchtools.PitchClassSegment([0, 1, 4, 7])
-            >>> timespans = pitch_operation_specifier.get_timespans(
-            ...     pitch_expr, 10)
-            >>> print(format(timespans))
-            supriya.tools.timetools.TimespanCollection(
-                [
-                    timespantools.AnnotatedTimespan(
-                        start_offset=durationtools.Offset(0, 1),
-                        stop_offset=durationtools.Offset(5, 2),
-                        annotation=pitchtools.PitchClassSegment(
-                            (
-                                pitchtools.NumberedPitchClass(1),
-                                pitchtools.NumberedPitchClass(6),
-                                pitchtools.NumberedPitchClass(7),
-                                pitchtools.NumberedPitchClass(10),
-                                ),
-                            item_class=pitchtools.NumberedPitchClass,
-                            ),
-                        ),
-                    timespantools.AnnotatedTimespan(
-                        start_offset=durationtools.Offset(5, 2),
-                        stop_offset=durationtools.Offset(15, 2),
-                        annotation=pitchtools.PitchClassSegment(
-                            (
-                                pitchtools.NumberedPitchClass(0),
-                                pitchtools.NumberedPitchClass(1),
-                                pitchtools.NumberedPitchClass(4),
-                                pitchtools.NumberedPitchClass(7),
-                                ),
-                            item_class=pitchtools.NumberedPitchClass,
-                            ),
-                        ),
-                    timespantools.AnnotatedTimespan(
-                        start_offset=durationtools.Offset(15, 2),
-                        stop_offset=durationtools.Offset(10, 1),
-                        annotation=pitchtools.PitchClassSegment(
-                            (
-                                pitchtools.NumberedPitchClass(11),
-                                pitchtools.NumberedPitchClass(2),
-                                pitchtools.NumberedPitchClass(5),
-                                pitchtools.NumberedPitchClass(10),
-                                ),
-                            item_class=pitchtools.NumberedPitchClass,
-                            ),
-                        ),
-                    ]
-                )
-
-        Returns timespan colleciton.
-        '''
-        pitch_expr_timespans = timetools.TimespanCollection()
-        operation_timespans = self._get_timespans(stop_offset)
-        for operation_timespan in operation_timespans:
-            pitch_operation = operation_timespan.annotation
-            if pitch_operation is not None:
-                operationed_pitch_expr = pitch_operation(pitch_expr)
-            else:
-                operationed_pitch_expr = pitch_expr
-            pitch_expr_timespan = new(
-                operation_timespan,
-                annotation=operationed_pitch_expr,
-                )
-            pitch_expr_timespans.insert(pitch_expr_timespan)
-        return pitch_expr_timespans
 
     ### PUBLIC PROPERTIES ###
 
