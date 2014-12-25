029eca507ab31aeb1dbb0cf1c12645ba76d84c66
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-24 20:37:40 -0500

    Made ChordExpression more flexible.

--- consort/tools/ChordExpression.py
+++ consort/tools/ChordExpression.py
@@ -16,12 +16,19 @@ class ChordExpression(LogicalTieExpression):
         >>> import consort
         >>> chord_expression = consort.ChordExpression(
         ...     arpeggio_direction=Down,
-        ...     interval_numbers=(-1, 3, 7),
+        ...     chord_expr=(-1, 3, 7),
         ...     )
         >>> print(format(chord_expression))
         consort.tools.ChordExpression(
             arpeggio_direction=Down,
-            interval_numbers=(-1, 3, 7),
+            chord_expr=pitchtools.IntervalSegment(
+                (
+                    pitchtools.NumberedInterval(-1),
+                    pitchtools.NumberedInterval(3),
+                    pitchtools.NumberedInterval(7),
+                    ),
+                item_class=pitchtools.NumberedInterval,
+                ),
             )
 
     ::
@@ -46,7 +53,7 @@ class ChordExpression(LogicalTieExpression):
 
     __slots__ = (
         '_arpeggio_direction',
-        '_interval_numbers',
+        '_chord_expr',
         )
 
     ### INITIALIZER ###
@@ -54,14 +61,18 @@ class ChordExpression(LogicalTieExpression):
     def __init__(
         self,
         arpeggio_direction=None,
-        interval_numbers=None,
+        chord_expr=None,
         ):
         assert arpeggio_direction in (Up, Down, Center, None)
-        if interval_numbers is not None:
-            interval_numbers = tuple(sorted(set(interval_numbers)))
-            assert len(interval_numbers)
+        if chord_expr is not None:
+            chord_expr = tuple(sorted(chord_expr))
+            assert len(chord_expr)
+            try:
+                chord_expr = pitchtools.IntervalSegment(chord_expr)
+            except:
+                chord_expr = pitchtools.PitchSegment(chord_expr)
         self._arpeggio_direction = arpeggio_direction
-        self._interval_numbers = interval_numbers
+        self._chord_expr = chord_expr
 
     ### SPECIAL METHODS ###
 
@@ -71,26 +82,11 @@ class ChordExpression(LogicalTieExpression):
         pitch_range=None,
         ):
         assert isinstance(logical_tie, selectiontools.LogicalTie), logical_tie
-        interval_numbers = self.interval_numbers or ()
-        interval_numbers = sorted(list(interval_numbers))
-        head = logical_tie.head
-        base_pitch = head.written_pitch
-        new_interval_numbers = interval_numbers
-        if pitch_range is not None:
-            assert base_pitch in pitch_range
-            maximum = max(interval_numbers)
-            minimum = min(interval_numbers)
-            maximum_pitch = base_pitch.transpose(maximum)
-            minimum_pitch = base_pitch.transpose(minimum)
-            if maximum_pitch not in pitch_range:
-                new_interval_numbers = [x - maximum for x in interval_numbers]
-            elif minimum_pitch not in pitch_range:
-                new_interval_numbers = [x - minimum for x in interval_numbers]
-        pitches = [base_pitch.transpose(x) for x in new_interval_numbers]
-        pitches = [pitchtools.NamedPitch(float(x)) for x in pitches]
-        if pitch_range is not None:
-            assert all(pitch in pitch_range for pitch in pitches), \
-                (pitch_range, base_pitch, interval_numbers, pitches)
+        if isinstance(self.chord_expr, pitchtools.IntervalSegment):
+            pitches = self._get_pitches_from_intervals(
+                logical_tie, pitch_range)
+        else:
+            pitches = self.chord_expr
         for i, leaf in enumerate(logical_tie):
             chord = scoretools.Chord(leaf)
             chord.written_pitches = pitches
@@ -99,6 +95,30 @@ class ChordExpression(LogicalTieExpression):
                 arpeggio = indicatortools.Arpeggio(self.arpeggio_direction)
                 attach(arpeggio, chord)
 
+    ### PRIVATE METHODS ###
+
+    def _get_pitches_from_intervals(self, logical_tie, pitch_range):
+        chord_expr = self.chord_expr or ()
+        head = logical_tie.head
+        base_pitch = head.written_pitch
+        new_chord_expr = chord_expr
+        if pitch_range is not None:
+            assert base_pitch in pitch_range
+            maximum = max(chord_expr)
+            minimum = min(chord_expr)
+            maximum_pitch = base_pitch.transpose(maximum)
+            minimum_pitch = base_pitch.transpose(minimum)
+            if maximum_pitch not in pitch_range:
+                new_chord_expr = [x - maximum for x in chord_expr]
+            elif minimum_pitch not in pitch_range:
+                new_chord_expr = [x - minimum for x in chord_expr]
+        pitches = [base_pitch.transpose(x) for x in new_chord_expr]
+        pitches = [pitchtools.NamedPitch(float(x)) for x in pitches]
+        if pitch_range is not None:
+            assert all(pitch in pitch_range for pitch in pitches), \
+                (pitch_range, base_pitch, chord_expr, pitches)
+        return pitches
+
     ### PUBLIC PROPERTIES ###
 
     @property
@@ -106,5 +126,5 @@ class ChordExpression(LogicalTieExpression):
         return self._arpeggio_direction
 
     @property
-    def interval_numbers(self):
-        return self._interval_numbers
+    def chord_expr(self):
+        return self._chord_expr
