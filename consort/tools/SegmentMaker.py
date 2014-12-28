c5aee3e8830017d82ef222b2835391d3ef701c32
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-28 13:21:24 -0800

    Adjusted SegmentMaker rehearsal mark formatting.

--- consort/tools/SegmentMaker.py
+++ consort/tools/SegmentMaker.py
@@ -262,9 +262,11 @@ class SegmentMaker(makertools.SegmentMaker):
     def attach_rehearsal_mark(self, score):
         first_leaf = score['TimeSignatureContext'].select_leaves()[0]
         if self.rehearsal_mark is not None:
-            markup_a = markuptools.Markup(
-                r'\concat {{ \vstrut "{}" }}'.format(str(self.rehearsal_mark)),
-                )
+            #markup_a = markuptools.Markup(
+            #    r'\concat {{ \vstrut "{}" }}'.format(str(self.rehearsal_mark)),
+            #    )
+            markup_a = markuptools.Markup(str(self.rehearsal_mark))
+            markup_a = markup_a.caps()
             markup_a = markup_a.box()
             markup_a = markup_a.override(('box-padding', 0.5))
             markup_b = markuptools.Markup('"{}"'.format(self.name or ' '))
