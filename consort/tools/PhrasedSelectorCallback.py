7ab11a5711b9a1bca786c3bf671c51c28be18f31
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-31 15:39:09 -0800

    PhrasedSelectorCallback now wraps all subresults in Selections.

--- consort/tools/PhrasedSelectorCallback.py
+++ consort/tools/PhrasedSelectorCallback.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import abctools
 from abjad.tools import durationtools
+from abjad.tools import selectiontools
 
 
 class PhrasedSelectorCallback(abctools.AbjadValueObject):
@@ -15,16 +16,20 @@ class PhrasedSelectorCallback(abctools.AbjadValueObject):
         assert isinstance(expr, tuple), repr(tuple)
         result = []
         for subexpr in expr:
+            subresult = []
             for division in subexpr[:-1]:
-                result.append(division.select_leaves()[0])
+                leaf = division.select_leaves()[0]
+                selection = selectiontools.Selection(leaf)
+                subresult.append(selection)
             leaves = subexpr[-1].select_leaves()
             if leaves.get_duration() <= durationtools.Duration(1, 8):
-                result.append(leaves[-1])
+                subresult.append(leaves[-1])
             elif 1 == len(leaves):
-                result.append(leaves[0])
+                subresult.append(leaves[0])
             else:
-                result.append(leaves[0])
-                result.append(leaves[1])
+                subresult.append(leaves[0])
+                subresult.append(leaves[1])
+            subresult = selectiontools.Selection(subresult)
+            result.append(subresult)
         result = tuple(result)
-        print(result)
         return result
