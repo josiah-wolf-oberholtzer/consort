8bb41340cc3c546b424d3f3406580a60d5c5d8f4
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-31 19:40:23 -0800

    Improved consort.debug().

--- consort/__init__.py
+++ consort/__init__.py
@@ -5,7 +5,7 @@ from consort.tools import *
 DEBUG = False
 
 
-def debug(message):
+def debug(*message):
     r'''Prints `message` in red when `consort.DEBUG` is true.
 
     Returns none.
@@ -13,6 +13,10 @@ def debug(message):
     RED = '\033[91m'
     END = '\033[0m'
     if DEBUG:
+        if len(message) == 1:
+            message = message[0]
+        else:
+            message = ' '.join(str(_) for _ in message)
         print(RED + str(message) + END)
 
 
