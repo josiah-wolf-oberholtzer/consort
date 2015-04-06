6af8fa18193d62d514aad1f23e000f1e3b95a337
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-05 23:59:03 -0700

    Extended DynamicExpression tests.

--- consort/tools/test/test_DynamicExpression.py
+++ consort/tools/test/test_DynamicExpression.py
@@ -405,6 +405,106 @@ def test_DynamicExpression_04():
 def test_DynamicExpression_05():
     dynamic_expression = consort.DynamicExpression(
         dynamic_tokens="f p mf mf mp",
+        stop_dynamic_tokens="ppp ppppp",
+        )
+
+    music = scoretools.Staff("{ c' d' } { e' f' } { g' a' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \p \<
+                d'4
+            }
+            {
+                e'4 \mf
+                f'4
+            }
+            {
+                g'4 \>
+                a'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' d' } { e' f' } { g' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \p \<
+                d'4
+            }
+            {
+                e'4 \mf \>
+                f'4
+            }
+            {
+                g'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' d' } { e' f' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \p \<
+                d'4
+            }
+            {
+                e'4 \mf \>
+                f'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' d' } { e' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \p \>
+                d'4
+            }
+            {
+                e'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' d' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \p \>
+                d'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \ppppp
+            }
+        }
+        ''')
+
+
+def test_DynamicExpression_06():
+    dynamic_expression = consort.DynamicExpression(
+        dynamic_tokens="f p mf mf mp",
         start_dynamic_tokens="fff fffff",
         stop_dynamic_tokens="ppp ppppp",
         )
@@ -498,6 +598,107 @@ def test_DynamicExpression_05():
         \new Staff {
             {
                 c'4 \fff
+            }
+        }
+        ''')
+
+
+def test_DynamicExpression_07():
+    dynamic_expression = consort.DynamicExpression(
+        dynamic_tokens="f p mf mf mp",
+        start_dynamic_tokens="fff fffff",
+        stop_dynamic_tokens="ppp ppppp",
+        )
+
+    music = scoretools.Staff("{ c' d' } { e' f' } { g' a' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \fffff \>
+                d'4
+            }
+            {
+                e'4 \p \<
+                f'4
+            }
+            {
+                g'4 \mf \>
+                a'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' d' } { e' f' } { g' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \fffff \>
+                d'4
+            }
+            {
+                e'4 \p \>
+                f'4
+            }
+            {
+                g'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' d' } { e' f' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \fffff \>
+                d'4
+            }
+            {
+                e'4 \p \>
+                f'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' d' } { e' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \fffff \>
+                d'4
+            }
+            {
+                e'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' d' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \fffff \>
+                d'4 \ppppp
+            }
+        }
+        ''')
+
+    music = scoretools.Staff("{ c' }")
+    dynamic_expression(music, seed=1)
+    assert format(music) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            {
+                c'4 \fffff
             }
         }
         ''')
