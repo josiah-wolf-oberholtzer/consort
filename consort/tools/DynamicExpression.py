196ae4ed6fa7565c26a6eb268cf9999036de8089
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-05 21:32:32 -0700

    Streamlined DynamicExpression.

--- consort/tools/DynamicExpression.py
+++ consort/tools/DynamicExpression.py
@@ -7,6 +7,7 @@ from abjad.tools import durationtools
 from abjad.tools import indicatortools
 from abjad.tools import lilypondnametools
 from abjad.tools import schemetools
+from abjad.tools import sequencetools
 from abjad.tools import spannertools
 
 
@@ -178,106 +179,171 @@ class DynamicExpression(abctools.AbjadValueObject):
     ### SPECIAL METHODS ###
 
     def __call__(self, music, name=None, seed=0):
+        original_seed = seed
         current_dynamic = None
         current_hairpin = None
-        selections = self._get_selections(music)
-        if 1 < len(selections):
-            for selection in selections[:-1]:
-                dynamic, hairpin, transition = self._get_attachments(seed)
-                #print('A', seed, selection, dynamic, hairpin, transition)
-                #print('?', self.dynamic_tokens[seed], self.dynamic_tokens)
-                if dynamic != current_dynamic:
-                    if current_hairpin is not None:
-                        current_hairpin._extend([selection[0]])
-                    current_dynamic = dynamic
-                    attach(dynamic, selection[0], name=name)
-                if hairpin is not None:
-                    current_hairpin = hairpin
-                    attach(hairpin, selection, name=name)
-                    hairpin_override = self._get_hairpin_override(
-                        transition)
-                    if hairpin_override is not None:
-                        attach(hairpin_override, selection[0], name=name)
-                elif current_hairpin is not None:
-                    #print('SELECTION', selection)
-                    #print('LEAVES', current_hairpin._get_leaves())
-                    if current_hairpin._is_my_last_leaf(selection[0]):
-                        current_hairpin._extend(selection[1:])
-                    else:
-                        current_hairpin._extend(selection)
-                seed += 1
-        dynamic, hairpin, transition = self._get_attachments(seed)
-        selection = selections[-1]
-        #print('B', seed, selection, dynamic, hairpin, transition)
-        #print('?', self.dynamic_tokens[seed], self.dynamic_tokens)
-        if selection.get_duration() <= durationtools.Duration(1, 8):
-            if current_hairpin is not None:
-                current_hairpin._extend(selection)
-                if dynamic != current_dynamic:
-                    attach(dynamic, selection[-1], name=name)
-            else:
-                attach(dynamic, selection[0], name=name)
-                if 1 < len(selection) and transition == 'constante':
-                    attach(hairpin, selection, name=name)
-                    hairpin_override = self._get_hairpin_override(transition)
-                    if hairpin_override is not None:
-                        attach(hairpin_override, selection[0], name=name)
-        else:
+        selections, components = self._get_selections(music)
+        length = len(components)
+        for index, component in enumerate(components[:-1]):
+            selection = selections[index]
+            dynamic, hairpin, hairpin_override = self._get_attachments(
+                index, length, seed, original_seed)
+            #print(index, dynamic, hairpin)
             if dynamic != current_dynamic:
-                if current_hairpin:
-                    current_hairpin._extend([selection[0]])
+                attach(dynamic, component)
                 current_dynamic = dynamic
-                attach(dynamic, selection[0], name=name)
-                if hairpin is not None:
-                    current_hairpin = hairpin
-                    if 1 < len(selection):
-                        attach(hairpin, selection, name=name)
-                        hairpin_override = self._get_hairpin_override(transition)
-                        if hairpin_override is not None:
-                            attach(hairpin_override, selection[0], name=name)
-            elif current_hairpin is not None:
-                current_hairpin._extend(selection)
+            if hairpin is not None:
+                attach(hairpin, selection)
+                current_hairpin = hairpin
+            if current_hairpin is not None and hairpin_override is not None:
+                attach(hairpin_override, component)
             seed += 1
-            dynamic, _, _ = self._get_attachments(seed)
-            #print('C', seed, selection, dynamic, hairpin, transition)
-            #print('?', self.dynamic_tokens[seed], self.dynamic_tokens)
-            if dynamic != current_dynamic and 1 < len(selection):
-                attach(dynamic, selection[-1], name=name)
-        #print()
+        dynamic, _, _ = self._get_attachments(
+            length - 1, length, seed, original_seed)
+        attach(dynamic, components[-1])
 
     ### PRIVATE METHODS ###
 
-    def _get_attachments(self, i):
-        this_dynamic = indicatortools.Dynamic(self.dynamic_tokens[i])
-        next_dynamic = indicatortools.Dynamic(self.dynamic_tokens[i + 1])
+    def _get_attachments(self, index, length, seed, original_seed):
+        dynamic_seed = seed
+        if self.start_dynamic_tokens:
+            dynamic_seed -= 1
+
+        this_token = self.dynamic_tokens[dynamic_seed]
+        next_token = self.dynamic_tokens[dynamic_seed + 1]
+        this_dynamic = indicatortools.Dynamic(this_token)
+        next_dynamic = indicatortools.Dynamic(next_token)
+
+        hairpin = None
+        hairpin_override = None
         if this_dynamic.ordinal < next_dynamic.ordinal:
             hairpin = spannertools.Crescendo(include_rests=True)
         elif next_dynamic.ordinal < this_dynamic.ordinal:
             hairpin = spannertools.Decrescendo(include_rests=True)
-        else:
-            hairpin = None
-        transition = self.transitions[i]
+        transition = self.transitions[seed]
         if transition == 'constante':
             hairpin = spannertools.Crescendo(include_rests=True)
-        return this_dynamic, hairpin, transition
-
-    def _get_hairpin_override(self, transition):
-        grob_override = None
         if transition in ('flared', 'constante'):
-            grob_override = lilypondnametools.LilyPondGrobOverride(
+            hairpin_override = lilypondnametools.LilyPondGrobOverride(
                 grob_name='Hairpin',
                 is_once=True,
                 property_path='stencil',
                 value=schemetools.Scheme('{}-hairpin'.format(transition)),
                 )
-        return grob_override
+        return this_dynamic, hairpin, hairpin_override
 
-    def _get_selections(self, music):
+    @staticmethod
+    def _get_selections(music):
+        r"""Gets selections and attach components from `music`.
+
+        ..  container:: example
+
+            ::
+
+                >>> music = Staff(r'''
+                ...     { c'4 d'4 e'4 f'4 }
+                ...     { g'4 a'4 b'4 }
+                ...     { c''4 }
+                ... ''')
+                >>> result = consort.DynamicExpression._get_selections(music)
+                >>> selections, attach_components = result
+                >>> for _ in selections:
+                ...     _
+                ...
+                ContiguousSelection(Note("c'4"), Note("d'4"), Note("e'4"), Note("f'4"), Note("g'4"))
+                ContiguousSelection(Note("g'4"), Note("a'4"), Note("b'4"), Note("c''4"))
+
+            ::
+
+                >>> for _ in attach_components:
+                ...     _
+                ...
+                Note("c'4")
+                Note("g'4")
+                Note("c''4")
+
+        ..  container:: example
+
+            ::
+
+                >>> music = Staff(r'''
+                ...     { c'4 d'4 e'4 }
+                ...     { f'4 g'4 a'4 }
+                ...     { b'4 c''4 }
+                ... ''')
+                >>> result = consort.DynamicExpression._get_selections(music)
+                >>> selections, attach_components = result
+                >>> for _ in selections:
+                ...     _
+                ...
+                ContiguousSelection(Note("c'4"), Note("d'4"), Note("e'4"), Note("f'4"))
+                ContiguousSelection(Note("f'4"), Note("g'4"), Note("a'4"), Note("b'4"))
+                ContiguousSelection(Note("b'4"), Note("c''4"))
+
+            ::
+
+                >>> for _ in attach_components:
+                ...     _
+                ...
+                Note("c'4")
+                Note("f'4")
+                Note("b'4")
+                Note("c''4")
+
+        ..  container:: example
+
+            ::
+
+                >>> music = Staff(r'''
+                ...     { c'8 d'8 e'8 }
+                ...     { f'8 g'8 a'8 }
+                ...     { b'32 c''16. }
+                ... ''')
+                >>> result = consort.DynamicExpression._get_selections(music)
+                >>> selections, attach_components = result
+                >>> for _ in selections:
+                ...     _
+                ...
+                ContiguousSelection(Note("c'8"), Note("d'8"), Note("e'8"), Note("f'8"))
+                ContiguousSelection(Note("f'8"), Note("g'8"), Note("a'8"), Note("b'32"), Note("c''16."))
+
+            ::
+
+                >>> for _ in attach_components:
+                ...     _
+                ...
+                Note("c'8")
+                Note("f'8")
+                Note("c''16.")
+
+        """
+        attach_components = []
         selections = []
-        for division in music:
+        for i, division in enumerate(music):
             selection = division.select_leaves()
-            selections.append(selection)
-        return selections
+            if i < len(music) - 1:
+                selection = selection + music[i + 1].select_leaves()[:1]
+                selections.append(selection)
+                attach_components.append(selection[0])
+            elif (
+                selection.get_duration() <= durationtools.Duration(1, 8) or
+                len(selection) == 1
+                ):
+                attach_components.append(selection[-1])
+                if selections:
+                    selections[-1] = selections[-1] + selection[1:]
+            elif (
+                    durationtools.Duration(1, 8) < (
+                    selection[-1]._get_timespan().start_offset -
+                    selection[0]._get_timespan().start_offset
+                        )
+                    ):
+                selections.append(selection)
+                attach_components.append(selection[0])
+                attach_components.append(selection[-1])
+            else:
+                attach_components.append(selection[0])
+        return selections, attach_components
 
     ### PUBLIC PROPERTIES ###
 
