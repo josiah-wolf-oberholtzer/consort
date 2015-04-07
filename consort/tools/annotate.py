694272374c565bd4e5299fc8b4e00800ea931c4b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-07 15:27:57 -0700

    Cleaned-up consort.annotate() function.
    The function can now be forced to not annotate silent phrases.

--- consort/tools/annotate.py
+++ consort/tools/annotate.py
@@ -1,28 +1,35 @@
 # -*- encoding: utf-8 -*-
-from abjad import *
+import consort
+from abjad import attach
+from abjad import inspect_
+from abjad import iterate
+from abjad import override
+from abjad.tools import durationtools
+from abjad.tools import scoretools
+from abjad.tools import schemetools
 
 
 def make_annotated_phrase(phrase):
     duration = inspect_(phrase).get_duration()
     if duration != 1:
-        note = Note("c'''1")
-        annotated_phrase = Tuplet(duration, (note,))
+        note = scoretools.Note("c'''1")
+        annotated_phrase = scoretools.Tuplet(duration, (note,))
     else:
         note = scoretools.Note("c'''2")
-        annotated_phrase = Tuplet(2, (note,))
+        annotated_phrase = scoretools.Tuplet(2, (note,))
     return annotated_phrase
 
 
 def make_annotated_division(division):
     duration = inspect_(division).get_duration()
     if duration != 1:
-        note = Note("c'''1")
-        annotated_division = Tuplet(duration, (note,))
+        note = scoretools.Note("c'''1")
+        annotated_division = scoretools.Tuplet(duration, (note,))
     else:
         note = scoretools.Note("c'''2")
-        annotated_division = Tuplet(2, (note,))
+        annotated_division = scoretools.Tuplet(2, (note,))
     leaves = division.select_leaves()
-    prototype = (Rest, scoretools.MultimeasureRest)
+    prototype = (scoretools.Rest, scoretools.MultimeasureRest)
     if all(isinstance(_, prototype) for _ in leaves):
         manager = override(annotated_division)
         manager.tuplet_bracket.dash_fraction = 0.1
@@ -32,11 +39,23 @@ def make_annotated_division(division):
     return annotated_division
 
 
-def annotate(context):
-    #annotated_context = mutate(context).copy()
+def make_empty_phrase(phrase):
+    skip = scoretools.Skip(1)
+    duration = inspect_(phrase).get_duration()
+    multiplier = durationtools.Multiplier(duration)
+    attach(multiplier, skip)
+    container = scoretools.Container([skip])
+    return container
+
+
+def annotate(context, nonsilence=None):
+    prototype = consort.MusicSpecifier
+    silence_specifier = consort.MusicSpecifier()
     annotated_context = context
     context_mapping = {}
-    for voice in iterate(annotated_context).by_class(Voice):
+    for voice in iterate(annotated_context).by_class(scoretools.Voice):
+        if voice.context_name == 'Dynamics':
+            continue
         division_voice = scoretools.Context(
             context_name='AnnotatedDivisionsVoice',
             )
@@ -44,6 +63,14 @@ def annotate(context):
             context_name='AnnotatedPhrasesVoice',
             )
         for phrase in voice:
+            if nonsilence:
+                music_specifier = inspect_(phrase).get_indicator(prototype)
+                if music_specifier == silence_specifier:
+                    annotated_divisions = make_empty_phrase(phrase)
+                    division_voice.append(annotated_divisions)
+                    annotated_phrase = make_empty_phrase(phrase)
+                    phrase_voice.append(annotated_phrase)
+                    continue
             for division in phrase:
                 annotation_division = make_annotated_division(division)
                 division_voice.append(annotation_division)
