fb4428ed591519537d62cb08bdbb026a4361d92f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-07 19:03:47 -0700

    Updated installation files.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
+import sys
 from distutils.core import setup
+from distutils.version import StrictVersion
 
-install_requires = (
+install_requires = [
     'abjad',
-    )
+    ]
+
+version = '.'.join(str(x) for x in sys.version_info[:3])
+if StrictVersion(version) < StrictVersion('3.3.0'):
+    install_requires.append('funcsigs')
 
 
 def main():
