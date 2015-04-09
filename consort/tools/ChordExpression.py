b893dc9db23d3f35042fc37f559635ac5a0548e7
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-09 00:02:30 -0700

    Implemented chord respelling magic.

--- consort/tools/ChordExpression.py
+++ consort/tools/ChordExpression.py
@@ -118,6 +118,130 @@ class ChordExpression(LogicalTieExpression):
 
     ### PRIVATE METHODS ###
 
+    @staticmethod
+    def _score_pitch_set(pitch_set):
+        buckets = {}
+        for pitch in pitch_set:
+            if pitch.diatonic_pitch_number not in buckets:
+                buckets[pitch.diatonic_pitch_number] = set()
+            buckets[pitch.diatonic_pitch_number].add(pitch)
+        penalty = 0
+        for diatonic_pitch_number, bucket in buckets.items():
+            penalty = penalty + (len(bucket) - 1)
+        return penalty
+
+    @staticmethod
+    def _flip_accidental(pitch):
+        if not pitch.alteration_in_semitones:
+            return pitch
+        elif 0 < pitch.alteration_in_semitones:
+            return pitch.respell_with_flats()
+        return pitch.respell_with_sharps()
+
+    @staticmethod
+    def _reshape_pitch_set(pitch_set):
+        altered = {}
+        unaltered = {}
+        for pitch in pitch_set:
+            diatonic_pitch_number = pitch.diatonic_pitch_number
+            if pitch.alteration_in_semitones:
+                if diatonic_pitch_number not in altered:
+                    altered[diatonic_pitch_number] = []
+                altered[diatonic_pitch_number].append(pitch)
+            else:
+                unaltered[diatonic_pitch_number] = pitch
+        new_altered = {}
+        for diatonic_pitch_number, altered_pitches in altered.items():
+            while altered_pitches:
+                altered_pitch = altered_pitches.pop()
+                if diatonic_pitch_number not in unaltered and \
+                    diatonic_pitch_number not in new_altered:
+                    if diatonic_pitch_number not in new_altered:
+                        new_altered[diatonic_pitch_number] = []
+                    new_altered[diatonic_pitch_number].append(altered_pitch)
+                else:
+                    new_pitch = ChordExpression._flip_accidental(altered_pitch)
+                    new_diatonic_pitch_number = new_pitch.diatonic_pitch_number
+                    if new_diatonic_pitch_number not in new_altered:
+                        new_altered[new_diatonic_pitch_number] = []
+                    new_altered[new_diatonic_pitch_number].append(new_pitch)
+        result = set(unaltered.values())
+        for altered_pitches in new_altered.values():
+            result.update(altered_pitches)
+        result = pitchtools.PitchSet(result)
+        return result
+
+    @staticmethod
+    def _respell_pitch_set(pitch_set):
+        r'''Respell pitch set.
+
+        ::
+
+            >>> pitch_set = pitchtools.PitchSet("c' e' g'")
+            >>> consort.ChordExpression._respell_pitch_set(pitch_set)
+            PitchSet(["c'", "e'", "g'"])
+
+        ::
+
+            >>> pitch_set = pitchtools.PitchSet("c' e' g' c'' cs'' g''")
+            >>> consort.ChordExpression._respell_pitch_set(pitch_set)
+            PitchSet(["c'", "e'", "g'", "c''", "df''", "g''"])
+
+        ::
+
+            >>> pitch_set = pitchtools.PitchSet("bf d' f' fs' bf' f''")
+            >>> consort.ChordExpression._respell_pitch_set(pitch_set)
+            PitchSet(['bf', "d'", "f'", "gf'", "bf'", "f''"])
+
+        ::
+
+            >>> pitch_set = pitchtools.PitchSet("b' c'' cs'' d''")
+            >>> consort.ChordExpression._respell_pitch_set(pitch_set)
+            PitchSet(["b'", "c''", "cs''", "d''"])
+
+        ::
+
+            >>> pitch_set = pitchtools.PitchSet("cf' c' cs'")
+            >>> consort.ChordExpression._respell_pitch_set(pitch_set)
+            PitchSet(['b', "c'", "df'"])
+
+        ::
+
+            >>> pitch_set = pitchtools.PitchSet("e ff f fs g")
+            >>> consort.ChordExpression._respell_pitch_set(pitch_set)
+            PitchSet(['e', 'f', 'gf', 'g'])
+
+        '''
+        initial_score = ChordExpression._score_pitch_set(pitch_set)
+        if not initial_score:
+            return pitch_set
+        flat_pitch_set = pitchtools.PitchSet(
+            _.respell_with_flats()
+            for _ in pitch_set
+            )
+        flat_score = ChordExpression._score_pitch_set(flat_pitch_set)
+        if not flat_score:
+            return flat_pitch_set
+        sharp_pitch_set = pitchtools.PitchSet(
+            _.respell_with_sharps()
+            for _ in pitch_set
+            )
+        sharp_score = ChordExpression._score_pitch_set(sharp_pitch_set)
+        if not sharp_score:
+            return sharp_pitch_set
+        scored_pitch_sets = [
+            (initial_score, pitch_set),
+            (flat_score, flat_pitch_set),
+            (sharp_score, sharp_pitch_set),
+            ]
+        scored_pitch_sets.sort()
+        reshaped_pitch_set = ChordExpression._reshape_pitch_set(
+            scored_pitch_sets[0][1])
+        reshaped_score = ChordExpression._score_pitch_set(reshaped_pitch_set)
+        scored_pitch_sets.append((reshaped_score, reshaped_pitch_set))
+        scored_pitch_sets.sort()
+        return scored_pitch_sets[0][1]
+
     def _get_pitches_from_intervals(self, base_pitch, pitch_range):
         chord_expr = self.chord_expr or ()
         new_chord_expr = chord_expr
