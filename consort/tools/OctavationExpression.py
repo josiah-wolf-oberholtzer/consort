07b47f658799ac55c00c6d2e27c5f934cbd9eb31
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-15 15:49:46 -0700

    Added OctavationExpression.

---
+++ consort/tools/OctavationExpression.py
@@ -0,0 +1,56 @@
+# -*- encoding: utf-8 -*-
+from __future__ import print_function
+from abjad import attach
+from abjad import inspect_
+from abjad.tools import abctools
+from abjad.tools import indicatortools
+from abjad.tools import pitchtools
+from abjad.tools import scoretools
+from abjad.tools import spannertools
+
+
+class OctavationExpression(abctools.AbjadValueObject):
+    r'''An octavation expression.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
+
+    ### INITIALIZER ###
+
+    def __init__(self):
+        pass
+
+    ### SPECIAL METHODS ###
+
+    def __call__(self, music, name=None):
+        leaves = music.select_leaves()
+        weights = []
+        weighted_pitches = []
+        for leaf in leaves:
+            weight = float(inspect_(leaf).get_duration())
+            if isinstance(leaf, scoretools.Note):
+                pitch = float(leaf.written_pitch)
+                weighted_pitch = pitch * weight
+                weights.append(weight)
+                weighted_pitches.append(weighted_pitch)
+            elif isinstance(leaf, scoretools.Chord):
+                for pitch in leaf.written_pitches:
+                    pitch = float(pitch)
+                    weighted_pitch = pitch * weight
+                    weighted_pitches.append(weighted_pitch)
+                    weights.append(weight)
+        sum_of_weights = sum(weights)
+        sum_of_weighted_pitches = sum(weighted_pitches)
+        weighted_average = sum_of_weighted_pitches / sum_of_weights
+        print(music, weighted_average)
+        clef = inspect_(leaves[0]).get_effective(indicatortools.Clef)
+        octavation_spanner = None
+        if clef == indicatortools.Clef('treble'):
+            if int(pitchtools.NamedPitch('C6')) <= int(weighted_average):
+                octavation_spanner = spannertools.OctavationSpanner()
+        elif clef == indicatortools.Clef('bass'):
+            pass
+        if octavation_spanner is not None:
+            attach(octavation_spanner, music)
