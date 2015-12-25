107a0346815973d36be39cb33525ed3135615b31
Josiah Oberholtzer <joberholtzer@discogsinc.com>
2015-12-25 16:27:29 -0500

    Modernizing Consort.

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
-
-import sys
 from distutils.core import setup
-from distutils.version import StrictVersion
+
 
 install_requires = [
     'abjad[development]',
+    'funcsigs',
     ]
-
-version = '.'.join(str(x) for x in sys.version_info[:3])
-if StrictVersion(version) < StrictVersion('3.3.0'):
-    install_requires.append('funcsigs')
 
 
 def main():
